5a9e325819dbe1fbed787015086524b332c9807f
nihalasri <nihalasri1410@gmail.com>
2026-01-15 13:01:35 +0530

    ready for cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,13 +6,8 @@ const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			precompress: false,
-			strict: true
+			// See https://kit.svelte.dev/docs/adapter-cloudflare#options
+			// for available options
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH || ''
